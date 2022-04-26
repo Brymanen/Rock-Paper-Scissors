e1796eea5d7ada923630857b55333a150fe3e8f5
Brymanen <mondschatten25@web.de>
2022-04-26 12:52:25 +0200

    adding the messages is completely done

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -11,35 +11,25 @@ class RockPaperScissors:
 		pygame.init()
 		pygame.display.set_caption("Rock Paper Scissors")
 		pygame.font.init()
-
-		# Import the settings
+		# Create an instance of Settings
 		self.settings = Settings()
-
 		# Display the game in fullscreen and grab the width and height of the game window
 		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 		self.screen_width = self.screen.get_rect().width
 		self.screen_height = self.screen.get_rect().height
-		
-
-		# create an empty list to store the all messages
-		#self.list_of_messages = []
+		# create an empty list to store the all played games, assign the number of played games to 0 and create a list to hold all messages
 		self.played_games = []
 		self.game_number = 0
 		self.messages = []
-
 		# Assign a font and font size
 		self.text_font = pygame.font.SysFont(pygame.font.get_default_font(), 30)
 
-
 	def run_game(self):
 		"""Start the main loop for the game."""
-		self.create_greeting_message()
 		while True:
 			self._check_events()
 			self._update_screen()
         	
-        	
-	# Watch for keyboard and mouse events.
 	def _check_events(self):
 		"""Respond to keypresses and mouse events."""
 		for event in pygame.event.get():
@@ -47,38 +37,36 @@ class RockPaperScissors:
 				sys.exit()
 			elif event.type == pygame.KEYDOWN:
 				self._check_keydown_events(event)
-                
 
-	def create_greeting_message(self):
+	def display_greeting_message(self):
 		"""Displays a text message on the screen"""
-		self.greeting_message = self.text_font.render(self.settings.greeting_message, True, self.settings.font_color)
+		message_content = self.settings.greeting_message
+		message = self.text_font.render(message_content, True, self.settings.font_color)
+		self.message_y += self.screen_width * self.settings.pos_y_spacing 
+		self.screen.blit(message,(self.message_x, self.message_y))
 
 	def respond_to_user_input(self):
+		"""The user can input rock, paper, scissors and the game receives the input and responds to it"""
 		# Randomly select a key from the dictioniary which maps rock, paper scissors to the number 0, 1, 2
 		self.computer_choice = random.choice(list(self.settings.rock_paper_scissors_mapping.keys()))
 		self.determine_result()
 		self.store_game_data()
-		#self.create_messages()
-
-
-
-
-
 
 	def determine_result(self):
-		# Determine the outcome of the game
+		"""Determines the outcome of the game"""
 		# If result = 0 then the game ended in a draw, if result = 1, the player won the game and if result = 2 the computer won the game
-		# Determine wether the game ended in a draw
+		# First, determine whether the game ended in a draw
 		if self.user_input == self.computer_choice:
 			self.result = 0
-		# Determine wether the player won
-		elif self.user_input == 0 and self.computer_choice == 2 or self.user_input == 2 and self.computer_choice == 1:
+		# Next, determine whether the player won
+		elif self.user_input == 0 and self.computer_choice == 2 or self.user_input == 2 and self.computer_choice == 1 or self.user_input == 1 and self.computer_choice == 0:
 			self.result = 1
 		# Else the computer won the game
 		else:
 			self.result = 2
 
 	def store_game_data(self):
+		"""Stores the game data in dictionaries. The dictionaries are being stored in a list."""
 		self.played_games.append(f"game_{self.game_number}")
 		self.played_games[self.game_number] = {
 		"game_number": self.game_number,
@@ -86,16 +74,29 @@ class RockPaperScissors:
 		"user_input": self.user_input,
 		"computer_choice": self.computer_choice 
 		}
-		print(self.played_games[self.game_number])
 		self.game_number += 1
 
-	#def create_messages(self):
-		
-		
+	def create_display_message(self, message_content):
+		"""Creates and displays the messages, will be blitted to the screen"""
+		message = self.text_font.render(message_content, True, self.settings.font_color)
+		self.message_y += self.screen_width * self.settings.pos_y_spacing 
+		# Blit the messages to the screen, but only if the messages will be displayed above a cutoff starting from the bottom of the screen
+		if self.message_y <= self.screen_height * self.settings.screen_bottom_cutoff:
+			# Blit the messages to the screen
+			self.screen.blit(message,(self.message_x, self.message_y))
 
-
-
-
+	def create_message_text(self, game):
+		# Create the string for the message for the message explaining explaining the game number to the list of messages
+		self.messages.append(f'{self.settings.text_game_number} {game["game_number"]+1}')
+		# Create the string for the message explaining the user input to the list of messages
+		self.messages.append(f'{self.settings.text_user_picked}{self.settings.rock_paper_scissors_mapping[game["user_input"]].title()}')
+		# Create the string for the message explaining the computer choice to the list of messages
+		self.messages.append(f'{self.settings.text_computer_picked}{self.settings.rock_paper_scissors_mapping[game["computer_choice"]].title()}')
+		# Create the string for the message explaining the game result to the list of messages
+		self.messages.append(f'{self.settings.text_result[game["result"]]}')
+		# Add a spacing between the messages of different games
+		self.message_y += self.screen_width * self.settings.pos_y_spacing_between_games
+	
 	def _check_keydown_events(self, event):
 		"""Respond to keypresses."""
 		if event.key == pygame.K_q:
@@ -119,22 +120,22 @@ class RockPaperScissors:
 		elif event.key == pygame.K_s:
 			self.user_input = None
 
-
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen."""
 		# Redraw the screen during each pass through the loop.
 		self.screen.fill(self.settings.background_color)
 		self.message_x = self.screen_width * self.settings.pos_x_spacing
 		self.message_y = self.screen_height * self.settings.pos_y_spacing
-
-		for game in self.played_games:
-			for game_key in game:
-				message = self.text_font.render(str(game[game_key]), True, self.settings.font_color)
-				self.message_y += self.screen_width * self.settings.pos_x_spacing 
-				print(f"self.message_y {self.message_y}")
-				self.screen.blit(message,(self.message_x, self.message_y))
-		
-
+		self.display_greeting_message()
+		# Loop through all played games, starting with the latest game
+		for game in reversed(self.played_games):
+			# Create the text for the messages for every game
+			self.create_message_text(game)
+			# Loop through all the messages 
+			for message in self.messages:
+				# Create the actual messages and display them
+				self.create_display_message(message)
+				self.messages = []
 		pygame.display.flip()
             
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,29 +3,33 @@ class Settings():
 		self.background_color = (255, 255, 255)
 		
 		# Store the greeting message
-		self.greeting_message = f"Let's play a game of rock paper scissors! "
-		self.greeting_message += "Press the key r for rock, p for paper and s for scissors."
+		self.greeting_message = f"Let's play rock, paper, scissors! "
+		self.greeting_message += "Press the key r for rock, p for paper and s for scissors. Press q to end to end the game."
 
 		# Messages explaining the user input
 		self.text_game_number = "This is game number: "
-		self.text_user_picked = "The user picked: "
+		self.text_user_picked = "You picked: "
 		self.text_computer_picked = "The computer picked: "
 
 		# Messages explaining the end of the game
-		self.text_draw = "The game ended in a draw!"
-		self.text_player_won = "You won the game!"
-		self.text_computer_won = "You lost the game!"
+		self.text_result = {
+			0: "The game ended in a draw!",
+			1: "You won the game!",
+			2: "You lost the game!"
+		}
 
 		#settings for the display of the messages
-		self.pos_x_spacing = 1/36
-		self.pos_y_spacing = 1/20
-		self.pos_y_spacing_top = 1/100
+		self.pos_x_spacing = 1/25
+		self.pos_y_spacing = 1/75
+		self.pos_y_spacing_top = 1/200
 		self.font_color = (0, 0, 0)
+		self.pos_y_spacing_between_games = 1/150
+		self.screen_bottom_cutoff = 0.9
 
 		# Mapping the choices of rock, paper, scissors to numbers in a dictionary
 		self.rock_paper_scissors_mapping = {
-		0: "rock",
-		1: "paper",
-		2: "scissors"
+			0: "rock",
+			1: "paper",
+			2: "scissors"
 		}
 
